b1d13ee65727a44519bb7f17528dd2632d676128
HARISHMALISETTY <malisettyharish@gmail.com>
2025-01-25 10:35:52 +0530

    new-functionality

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+let display = document.getElementById("display");
+function calc(event) {
+  
+  display.value += event.target.value;
+}
 
-let display=document.getElementById("display")
-function calc(event){
-    console.log(event.target.value)
+function my_evaluate() {
+  display.value = eval(display.value);
+}
 
+function del() {
+  display.value = display.value.slice(0, -1);
+}
+
+function allclear(){
+    display.value=""
 }
